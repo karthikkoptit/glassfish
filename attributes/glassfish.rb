b2bbbcd123079b614a7ace1536f8bddfe98a66fa
karthikkoptit <karthik.k@optit.co>
2016-04-13 04:25:43 +0530

    clean

--- attributes/glassfish.rb
+++ attributes/glassfish.rb
@@ -1,22 +1,3 @@
-#
-# Cookbook Name:: glassfish
-# Attribute File:: glassfish
-#
-# Copyright 2009, Opscode, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-#openInstaller Dry Run Answer File.  This File can be used as input to the openInstaller engine using the -a option.
 
 # unis system user
 set_unless[:glassfish][:systemuser]="glassfish"
